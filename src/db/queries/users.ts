732c630c8c6bb06441b8086e2071634219e831d9
baraafanah21 <baraafanah1@gmail.com>
2026-09-23 18:17:06 +0300

    Add user queries

---
+++ src/db/queries/users.ts
@@ -0,0 +1,44 @@
+import { eq } from "drizzle-orm";
+
+import { db } from "../index.js";
+import type { NewUser } from "../schema.js";
+import { users } from "../schema.js";
+
+export async function createUser(user: NewUser) {
+  const [result] = await db
+    .insert(users)
+    .values(user)
+    .onConflictDoNothing()
+    .returning();
+  return result;
+}
+
+export async function deleteAllUsers() {
+  await db.delete(users);
+}
+
+export async function getUserByEmail(email: string) {
+  const [result] = await db.select().from(users).where(eq(users.email, email));
+  return result;
+}
+
+export async function upgradeUserToChirpyRed(userId: string) {
+  const [result] = await db
+    .update(users)
+    .set({ isChirpyRed: true })
+    .where(eq(users.id, userId))
+    .returning();
+  return result;
+}
+
+export async function updateUser(
+  userId: string,
+  fields: Pick<NewUser, "email" | "hashedPassword">,
+) {
+  const [result] = await db
+    .update(users)
+    .set(fields)
+    .where(eq(users.id, userId))
+    .returning();
+  return result;
+}
